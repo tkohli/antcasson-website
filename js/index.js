bb0afcae0827c8747e64d67f079842ee859a247e
AntCasson <anthonyrcasson@gmail.com>
2022-01-17 10:18:52 +0100

    small nav working till 500px and 31.25rem

--- js/index.js
+++ js/index.js
@@ -1,12 +1,41 @@
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link')
+
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     })
+// })
+
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const nav = document.querySelector('#nav')
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+        nav.style.transform = "translateX(0)"
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+        nav.style.transform = "translateX(100%)"
+    }
 })
+
+
+
+
+// console.log(nav.getAttribute("data-visible"))
+
+// navToggle.addEventListener("click", () => {
+
+//     const open = nav.getAttribute = "data-visible"
+//     console.log(open)
+//     nav.style.transform = "translateX(100%)"
+// })
